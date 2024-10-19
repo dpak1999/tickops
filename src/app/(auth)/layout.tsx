fb153a382712fac9461df65b700bbab8de212a96
Deepak <deepakkumardash313@gmail.com>
2024-10-20 01:58:43 +0530

    Add : logo and default auth layout

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { FC, ReactNode } from "react";
+
+interface AuthLayoutProps {
+  children: ReactNode;
+}
+
+const AuthLayout: FC<AuthLayoutProps> = ({ children }) => {
+  return (
+    <main className="bg-neutral-100 min-h-screen">
+      <div className="mx-auto max-w-screen-2xl p-4">
+        <nav className="flex justify-between items-center">
+          <Image src="/logo.svg" height={42} width={48} alt="logo" />
+          <Button variant={"secondary"}>Sign up</Button>
+        </nav>
+
+        <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
+          {children}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default AuthLayout;
