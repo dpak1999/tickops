dd7db4ac32d1593edc2e888fdbaedafc7a98c299
Deepak <deepakkumardash313@gmail.com>
2024-11-24 01:38:13 +0530

    Remove errors and loaders

--- src/components/PageLoader.tsx
+++ src/components/PageLoader.tsx
@@ -2,7 +2,7 @@ import { Loader } from "lucide-react";
 
 const PageLoader = () => {
   return (
-    <div className="flex items-center justify-center h-full">
+    <div className="flex items-center justify-center h-screen">
       <Loader className="animate-spin size-6 text-muted-foreground" />
     </div>
   );
